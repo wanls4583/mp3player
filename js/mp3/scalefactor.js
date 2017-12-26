7d910e83d9c8640014ca13016d3c061ce073ccb8
wanls4583 <409051987@qq.com>
2017-12-26 22:45:49 +0800

    解码比例因子

---
+++ js/mp3/scalefactor.js
@@ -0,0 +1,100 @@
+/**
+ * MP3音频比例因子模块
+ */
+define(function(require, exports, module) {
+    'use strict';
+
+    var BitStream = require('../common/bitstream');
+    var SideInfo = require('./sideInfo');
+
+    var bitStream = null;
+    var sideInfo = null; 
+    var offset = 0; //比例因子数据偏移量
+
+    var slen0 = [ 0, 0, 0, 0, 3, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4 ];  
+    var slen1 = [ 0, 1, 2, 3, 0, 1, 2, 3, 1, 2, 3, 1, 2, 3, 2, 3 ];
+
+    function ScaleFactor(arrayBuffer){
+        sideInfo = new SideInfo(arrayBuffer)
+        offset = sideInfo.parseSideInfo();
+        sideInfo.part2_bits = [[],[]];
+        bitStream = new BitStream(arrayBuffer.slice(offset));
+        sideInfo.scfL = [[],[]]; // [2][23];
+        sideInfo.scfS = [[[],[]],[[],[]]]; // [2][3][13];
+    }
+
+    var _proto_ = ScaleFactor.prototype;
+
+    _proto_.parseScaleFactors = function(gr, ch){
+        if(!offset){
+            return false;
+        }
+        var scale_comp = sideInfo.scalefac_compress[gr][ch];  
+        var length0 = slen0[scale_comp];  
+        var length1 = slen1[scale_comp];  
+        var sfb, win;
+        var l = sideInfo.scfL[ch];  
+        var s = sideInfo.scfS[ch];
+        sideInfo.part2_bits[gr][ch] = 0;  
+      
+        if (sideInfo.window_switching_flag[gr][ch] != 0 && sideInfo.block_type[gr][ch] == 2) {  
+            if (sideInfo.mixed_block_flag[gr][ch] != 0) {  
+                // MIXED block  
+                sideInfo.part2_bits[gr][ch] = 17 * length0 + 18 * length1;  
+                for (sfb = 0; sfb < 8; sfb++)  
+                    l[sfb] = bitStream.getBits(length0);  
+      
+                for (sfb = 3; sfb < 6; sfb++)  
+                    for (win = 0; win < 3; win++)  
+                        s[win][sfb] = bitStream.getBits(length0);  
+      
+                for (sfb = 6; sfb < 12; sfb++)  
+                    for (win = 0; win < 3; win++)  
+                        s[win][sfb] = bitStream.getBits(length1);  
+            } else {  
+                // pure SHORT block  
+                sideInfo.part2_bits[gr][ch] = 18 * (length0 + length1);  
+                for (sfb = 0; sfb < 6; sfb++)  
+                    for (win = 0; win < 3; win++)  
+                        s[win][sfb] = bitStream.getBits(length0);  
+                for (sfb = 6; sfb < 12; sfb++)  
+                    for (win = 0; win < 3; win++)  
+                        s[win][sfb] = bitStream.getBits(length1);  
+            }  
+        } else {  
+            // LONG types 0,1,3  
+            var scfsi = sideInfo.scfsi[ch];  
+            if (gr == 0) {  
+                sideInfo.part2_bits[gr][ch] = 10 * (length0 + length1) + length0;  
+                for (sfb = 0; sfb < 11; sfb++)  
+                    l[sfb] = bitStream.getBits(length0);  
+                for (sfb = 11; sfb < 21; sfb++)  
+                    l[sfb] = bitStream.getBits(length1);  
+            } else {  
+                sideInfo.part2_bits[gr][ch] = 0;  
+                if (scfsi[0] == 0) {  
+                    for (sfb = 0; sfb < 6; sfb++)  
+                        l[sfb] = bitStream.getBits(length0);  
+                    sideInfo.part2_bits[gr][ch] += 6 * length0;  
+                }  
+                if (scfsi[1] == 0) {  
+                    for (sfb = 6; sfb < 11; sfb++)  
+                        l[sfb] = bitStream.getBits(length0);  
+                    sideInfo.part2_bits[gr][ch] += 5 * length0;  
+                }  
+                if (scfsi[2] == 0) {  
+                    for (sfb = 11; sfb < 16; sfb++)  
+                        l[sfb] = bitStream.getBits(length1);  
+                    sideInfo.part2_bits[gr][ch] += 5 * length1;  
+                }  
+                if (scfsi[3] == 0) {  
+                    for (sfb = 16; sfb < 21; sfb++)  
+                        l[sfb] = bitStream.getBits(length1);  
+                    sideInfo.part2_bits[gr][ch] += 5 * length1;  
+                }  
+            }  
+        }
+        return sideInfo;
+    }
+    return ScaleFactor;
+})
--- js/mp3/sideinfo.js
+++ js/mp3/sideinfo.js
@@ -5,27 +5,116 @@ define(function(require, exports, module) {
     'use strict';
 
     var BitStream = require('../common/bitstream');
-
+    
+    var HEADER_MASK = 0xffe0 >> 5; //同步头
+    var MAX_TAG_OFF = 10 * 1024; //查找帧头，最多查找10K
     var bitStream = null;
-    var main_data_begin = 0; //主数据开始位置偏移量(<=0)
-    var private_bits = 0; //私有位(ISO在以后将不适用这些位)
-    var scfsi = []; //scfsi[ch][scfsi_band]，scfsi控制着比例因子到颗粒中的使用
-    var part2_3_length = []; //part2_3_length[gr][ch]，该值包含了主数据中比例因子和哈弗曼编码使用的比特数
-    var bit_values = []; //bit_values[gr][ch]，大值区域个数
-    var global_gain= []; //global_gain[gr][ch]，量化步长
-    var scalefac_compress = []; //scalefac_compress[gr][ch]，用来选择比例因子所使用的比特位个数
-    var window_switching_flag = []; //window_switching_flag[gr][ch]，用来表示使用了不是标准窗(type 0)的标志
-    var block_type = []; //block_type[gr][ch]，指出实际颗粒的加窗函数类型
-    var mixed_block_flag = []; //mixed_block_flag[gr][ch]，窗转换类型标志
-    var table_select = []; //table_select[gr][ch][region]，哈弗曼编码表选择信息
-    var subblock_gain = []; //subblock_gain[gr][ch][windwo]，子块增益偏移
-    var region0_count = []; //region0_count[gr][ch]，bit_values区域细分
-    var region1_count = []; //region1_count[gr][ch]，bit_values区域细分
-    var preflag = []; //preflag[gr][ch]，量化相关标志
-    var scalefac_scale = []; //scalefac_scale[gr][ch]，比例因子乘数
-    var count1table_select = []; //count1table_select[gr][ch]，count1区域哈弗曼编码表选择信息
+    var channels = 0; //声道数
 
-    function SideInfo(arrayBuffer){
-
+    function SideInfo(arrayBuffer) {
+        bitStream = new BitStream(arrayBuffer);
+        /*帧变信息-BEGIN*/
+        this.main_data_begin = 0; //主数据开始位置偏移量(<=0)
+        this.private_bits = 0; //私有位(ISO在以后将不适用这些位)
+        this.scfsi = []; //scfsi[ch][scfsi_band]，scfsi控制着比例因子到颗粒中的使用
+        this.part2_3_length = [[],[]]; //part2_3_length[gr][ch]，该值包含了主数据中比例因子和哈弗曼编码使用的比特数
+        this.big_values = [[],[]]; //bit_values[gr][ch]，大值区域个数
+        this.global_gain = [[],[]]; //global_gain[gr][ch]，量化步长
+        this.scalefac_compress = [[],[]]; //scalefac_compress[gr][ch]，用来选择比例因子所使用的比特位个数
+        this.window_switching_flag = [[],[]]; //window_switching_flag[gr][ch]，用来表示使用了不是标准窗(type 0)的标志
+        this.block_type = [[],[]]; //block_type[gr][ch]，指出实际颗粒的加窗函数类型
+        this.mixed_block_flag = [[],[]]; //mixed_block_flag[gr][ch]，窗转换类型标志
+        this.table_select = [[[],[]],[[],[]]]; //table_select[gr][ch][region]，哈弗曼编码表选择信息
+        this.subblock_gain = [[[],[]],[[],[]]]; //subblock_gain[gr][ch][windwo]，子块增益偏移
+        this.region0_count = [[],[]]; //region0_count[gr][ch]，bit_values区域细分
+        this.region1_count = [[],[]]; //region1_count[gr][ch]，bit_values区域细分
+        this.preflag = [[],[]]; //preflag[gr][ch]，量化相关标志
+        this.scalefac_scale = [[],[]]; //scalefac_scale[gr][ch]，比例因子乘数
+        this.count1table_select = [[],[]]; //count1table_select[gr][ch]，count1区域哈弗曼编码表选择信息
+        /*帧变信息-END*/
     }
+
+    var _proto_ = SideInfo.prototype;
+
+    _proto_.parseSideInfo = function() {
+        bitStream.reset(); //指针指向头部
+        var mask = 0;
+        var channelMode = 0;
+        var offset = 0;
+        channels = 0;
+        do {
+            mask = bitStream.getBits(11); //获取是11位同步头
+            if (mask != HEADER_MASK) {
+                if (bitStream.isEnd()) {
+                    break;
+                }
+                bitStream.rewindBits(3);
+                continue;
+            }
+            bitStream.skipBits(24 - 11);
+            channelMode = bitStream.getBits(2);
+            bitStream.skipBits(32 - 26);
+            offset = bitStream.getBytePos();
+            this.main_data_begin = bitStream.getBits(9);
+            if (channelMode == 3){
+                channels = 1;
+                bitStream.getBits(5); //private_bits  
+            }else{
+                channels = 2;
+                bitStream.getBits(3); //private_bits  
+            }
+            for (ch = 0; ch < channels; ch++) {
+                this.scfsi[0] = bitStream.getBits1();
+                this.scfsi[1] = bitStream.getBits1();
+                this.scfsi[2] = bitStream.getBits1();
+                this.scfsi[3] = bitStream.getBits1();
+            }
+            for (var gr = 0; gr < 2; gr++) {
+                for (var ch = 0; ch < channels; ch++) {
+                    this.part2_3_length[gr][ch] = bitStream.getBits(12);
+                    this.big_values[gr][ch] = bitStream.getBits(9);
+                    this.global_gain[gr][ch] = bitStream.getBits(8);
+                    this.scalefac_compress[gr][ch] = bitStream.getBits(4);
+                    this.window_switching_flag[gr][ch] = bitStream.getBits1();
+                    if ((this.window_switching_flag[gr][ch]) != 0) {
+                        this.block_type[gr][ch] = bitStream.getBits(2);
+                        this.mixed_block_flag[gr][ch] = bitStream.getBits1();
+                        this.table_select[gr][ch][0] = bitStream.getBits(5);
+                        this.table_select[gr][ch][1] = bitStream.getBits(5);
+                        this.subblock_gain[gr][ch][0] = bitStream.getBits(3);
+                        this.subblock_gain[gr][ch][1] = bitStream.getBits(3);
+                        this.subblock_gain[gr][ch][2] = bitStream.getBits(3);
+                        if (this.block_type[gr][ch] == 0)
+                            return false;
+                        else if (this.block_type[gr][ch] == 2 && this.mixed_block_flag[gr][ch] == 0)
+                            this.region0_count[gr][ch] = 8;
+                        else
+                            this.region0_count[gr][ch] = 7;
+                        this.region1_count[gr][ch] = 20 - this.region0_count[gr][ch];
+                    } else {
+                        this.table_select[gr][ch][0] = bitStream.getBits(5);
+                        this.table_select[gr][ch][1] = bitStream.getBits(5);
+                        this.table_select[gr][ch][2] = bitStream.getBits(5);
+                        this.region0_count[gr][ch] = bitStream.getBits(4);
+                        this.region1_count[gr][ch] = bitStream.getBits(3);
+                        this.block_type[gr][ch] = 0;
+                    }
+                    this.preflag[gr][ch] = bitStream.getBits1();
+                    this.scalefac_scale[gr][ch] = bitStream.getBits1();
+                    this.count1table_select[gr][ch] = bitStream.getBits1();
+                }
+            }
+            if(channelMode==3){
+                offset+=17;
+            }else{
+                offset+=32;
+            }
+            return offset; //返回偏移量位置，拱后续解析比例因子
+
+        } while (bitStream.getBytePos() < MAX_TAG_OFF);
+
+        return false;
+    }
+
+    return SideInfo;
 })
